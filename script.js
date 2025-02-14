29eae35d4a17a8bbbd5ced8f790ce44c7f155b86
Vladislav Kraemer <v21kraemer@gmail.com>
2025-02-14 22:46:58 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
+const formBtn = itemForm.querySelector('button');
+let isEditMode = false;
+
+function displayItems() {
+  const itemsFromStorage = getItemsFromStorage();
+  itemsFromStorage.forEach((item) => addItemToDOM(item));
+  checkUI();
+}
+
+function onAddItemSubmit(e) {
+  e.preventDefault();
+
+  // trim the input value to remove whitespace - disallowing duplicate items due to white space in the process
+  const newItem = itemInput.value.trim();
+
+  // Validate Input
+  if (newItem === '') {
+    alert('Please add an item');
+    return;
+  }
