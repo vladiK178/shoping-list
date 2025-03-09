0670fed3dad76bbc053308d62a2df61044fd4d13
Vladislav Kraemer <v21kraemer@gmail.com>
2025-03-09 23:12:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const itemForm = document.getElementById('item-form');
+
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearBtn = document.getElementById('clear');
